bd605ceed5e998c93c545fd842e14c9d7c14bd74
haydenxay <haydenxay@gmail.com>
2020-10-01 15:27:21 +0800

    Update on admin portal

--- CGS-CIMB_TechnicalTestAdminPortal/Controllers/HomeController.cs
+++ CGS-CIMB_TechnicalTestAdminPortal/Controllers/HomeController.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CGS_CIMB_TechnicalTestAdminPortal.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CGS_CIMB_TechnicalTestAdminPortal.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public static string Baseurl = "https://localhost:44390/api/";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -24,9 +29,24 @@ namespace CGS_CIMB_TechnicalTestAdminPortal.Controllers
             return View(reports);
         }
 
-        public IActionResult Privacy()
+        [Authorize]
+        public IActionResult Privacy(string id)
         {
-            return View();
+            string localFilePath;
+
+            HttpClientHandler hndlr = new HttpClientHandler();
+            hndlr.UseDefaultCredentials = true;
+            var client = new HttpClient(hndlr) { BaseAddress = new Uri(Baseurl) };
+
+            client.BaseAddress = new Uri(Baseurl);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("Reports/" + id).Result;
+
+            localFilePath = response.Content.Headers.ContentDisposition.FileNameStar;
+
+            var stream = new FileStream(localFilePath, FileMode.Open);
+            return new FileStreamResult(stream, "application/pdf");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- CGS-CIMB_TechnicalTestAdminPortal/Controllers/Webservices.cs
+++ CGS-CIMB_TechnicalTestAdminPortal/Controllers/Webservices.cs
@@ -1,11 +1,16 @@
-﻿using CGS_CIMB_TechnicalTestAdminPortal.Models;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CGS_CIMB_TechnicalTestAdminPortal.Data;
+using CGS_CIMB_TechnicalTestAdminPortal.Models;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CGS_CIMB_TechnicalTestAdminPortal.Controllers
 {
@@ -27,21 +32,5 @@ namespace CGS_CIMB_TechnicalTestAdminPortal.Controllers
 
             return datareport;
         }
-
-        //public static ReportsDTO GetDataReports(string id)
-        //{
-        //    HttpClientHandler hndlr = new HttpClientHandler();
-        //    hndlr.UseDefaultCredentials = true;
-        //    var client = new HttpClient(hndlr) { BaseAddress = new Uri(Baseurl) };
-
-        //    client.BaseAddress = new Uri(Baseurl);
-        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //    HttpResponseMessage response = client.GetAsync("Reports").Result;
-        //    var stringData = response.Content.ReadAsStreamAsync().Result;
-        //    var datareport = JsonConvert.DeserializeObject<ReportsDTO>(stringData);
-
-        //    return datareport;
-        //}
-
     }
 }
